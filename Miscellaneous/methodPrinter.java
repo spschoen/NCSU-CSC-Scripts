0b6a5231d17e093246689eb403ac44e10a7b67c3
Samuel Schoeneberger <spschoen@ncsu.edu>
2016-11-13 23:05:03 -0500

    Creating methodPrinter.java
    Whose sole purpose is to be a more specific less/cat/more/etc

---
+++ Miscellaneous/methodPrinter.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * The programming equivalent of a lazy sunday.
+ * Provide CLI of a file to scan, and then give it a method name to search for
+ * and this program will print out the entire method.  It's stupid, but it works.
+ * It's best used with directoryOrder.sh
+ * @author Samuel Schoeneberger
+ */
+public class methodPrinter {
+	
+	/**
+	 * It starts the program.
+	 * @param args CLI arguments, should be 1 in length, the file to scan.
+	 */
+	public static void main(String[] args) {
+	
+		if ( args.length != 1 ) {
+			System.out.println("rip");
+			System.exit(1);
+		}
+	
+		Scanner in = new Scanner(System.in);
+		System.out.print("Method name: ");
+		String methodName = in.next();
+		
+		String dir = args[0];
+		File f = new File(dir);
+		if ( !f.exists() ) {
+			System.out.println("rip");
+			System.exit(1);
+		}
+		
+		Scanner fi = null;
+		try {
+			fi = new Scanner(f);
+		} catch (Exception e) {
+			System.out.println("rip");
+			System.exit(1);
+		}
+		
+		int bracketCount = 1;
+		boolean gottem = false;
+		while ( fi.hasNextLine() ) {
+			String s = fi.nextLine();
+			if ( ( s.contains("public") || s.contains("private") || s.conatins("protected") )
+					&& s.contains(methodName) ) {
+				gottem = true;
+			}
+			
+			if ( gottem ) {
+				System.out.println(s);
+				if (s.contains("{")) {
+					bracketCount++;
+				}
+				if (s.contains("}")) {
+					bracketCount--;
+				}
+			}
+			if ( bracketCount == 1 && gottem ) {
+				break;
+			}
+		}
+	
+	}
+	
+}
